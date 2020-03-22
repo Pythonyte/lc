4b9f0a3f92ddf98394a19d59fb47ab2242d6f794
Sumit Singhal <sumitsinghal4390@gmail.com>
2020-03-22 10:50:20 +0530

    Create validate_ip_address.py

---
+++ validate_ip_address.py
@@ -0,0 +1,49 @@
+class Solution:
+    """Regex Solution'
+    
+    import re
+    chunk_IPv4 = r'([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])'
+    patten_IPv4 = re.compile(r'^(' + chunk_IPv4 + r'\.){3}' + chunk_IPv4 + r'$')
+    
+    chunk_IPv6 = r'([0-9a-fA-F]{1,4})'
+    patten_IPv6 = re.compile(r'^(' + chunk_IPv6 + r'\:){7}' + chunk_IPv6 + r'$')
+
+    def validIPAddress(self, IP: str) -> str:        
+        if self.patten_IPv4.match(IP):
+            return "IPv4"
+        return "IPv6" if self.patten_IPv6.match(IP) else "Neither" 
+    
+    """
+    
+    def validate_IPv4(self, IP: str) -> str:
+        nums = IP.split('.')
+        for x in nums:
+            # Validate integer in range (0, 255):
+            # 1. length of chunk is between 1 and 3
+            if len(x) == 0 or len(x) > 3:
+                return "Neither"
+            # 2. no extra leading zeros
+            # 3. only digits are allowed
+            # 4. less than 255
+            if x[0] == '0' and len(x) != 1 or not x.isdigit() or int(x) > 255:
+                return "Neither"
+        return "IPv4"
+    
+    def validate_IPv6(self, IP: str) -> str:
+        nums = IP.split(':')
+        hexdigits = '0123456789abcdefABCDEF'
+        for x in nums:
+            # Validate hexadecimal in range (0, 2**16):
+            # 1. at least one and not more than 4 hexdigits in one chunk
+            # 2. only hexdigits are allowed: 0-9, a-f, A-F
+            if len(x) == 0 or len(x) > 4 or not all(c in hexdigits for c in x):
+                return "Neither"
+        return "IPv6"
+        
+    def validIPAddress(self, IP: str) -> str:
+        if IP.count('.') == 3:
+            return self.validate_IPv4(IP)
+        elif IP.count(':') == 7:
+            return self.validate_IPv6(IP)
+        else:
+            return "Neither"
