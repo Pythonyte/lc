58b9c39d3348d385d068dce59c72390212009bca
Sumit Singhal <sumitsinghal4390@gmail.com>
2020-03-31 17:49:04 +0530

    Update numIslands.py

--- numIslands.py
+++ numIslands.py
@@ -38,3 +38,29 @@ class Solution:
         count_islands()
         
         return islands
+
+  
+# Improved Solution
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        count = 0
+        
+        def dfs():
+            def explore(r, c):
+                if 0 <= r < len(grid) and 0 <= c < len(grid[0]) and grid[r][c] != 'T' and grid[r][c] != '0':
+                        grid[r][c] = 'T'
+                        explore(r+1, c)
+                        explore(r-1, c)
+                        explore(r, c+1)
+                        explore(r, c-1)
+
+            for r in range(len(grid)):
+                for c in range(len(grid[0])):
+                    if grid[r][c] == '1':
+                        nonlocal count
+                        count += 1
+                        explore(r, c)
+        
+        dfs()
+        print(grid)
+        return count
