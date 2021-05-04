63bb820f5e8bdbecac7e61e2155cde20d01c0988
Sumit Singhal <sumitsinghal4390@gmail.com>
2021-05-04 19:31:37 +0530

    Update Design-Trie.py

--- Design-Trie.py
+++ Design-Trie.py
@@ -68,3 +68,52 @@ t.add_word("monday")
 t.printTrie()
 print(t.search_word('mango'))
 print(t.search_word('msss'))
+
+
+
+
+
+
+
+# Optimized Simple Solution:
+class TrieNode():
+    def __init__(self):
+        self.children = collections.defaultdict(TrieNode)
+        self.isWord = False
+    
+class Trie():
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word):
+        node = self.root
+        for w in word:
+            node = node.children[w]
+        node.isWord = True
+    
+    def search(self, word):
+        node = self.root
+        for w in word:
+            node = node.children.get(w)
+            if not node:
+                return False
+        return node.isWord          
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        node = self.root
+        for w in prefix:
+            node = node.children.get(w)
+            if not node:
+                return False
+        else:
+            return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
