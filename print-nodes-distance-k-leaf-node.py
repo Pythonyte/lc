0f5aa3a4ffcf5ba2981e1997cb2244b4e64c3ef8
Sumit Singhal <sumitsinghal4390@gmail.com>
2021-05-05 20:14:03 +0530

    Update print-nodes-distance-k-leaf-node.py

--- print-nodes-distance-k-leaf-node.py
+++ print-nodes-distance-k-leaf-node.py
@@ -46,6 +46,54 @@ def printKDistantfromLeaf(node, k):
     kDistantFromLeafUtil(node, path, visited, 0, k)
     return ans
 
+###############################################################################################
+###############################################################################################
+###############################################################################################
+###############################################################################################
+###############################################################################################
+
+# Function to check if a given node is a leaf node or not
+def isLeaf(node):
+    return node.left is None and node.right is None
+
+
+# Recursive function to find all nodes at a given distance from leaf nodes
+def leafNodeDistance(node, path, set, dist):
+    # base case: empty tree
+    if node is None:
+        return
+
+    # if a leaf node is found, insert the node at a distance `dist` from the
+    # leaf node into the set
+    if isLeaf(node) and len(path) >= dist:
+        set.add(path[-dist])
+        return
+
+    # include the current node in the current path
+    path.append(node)
+    # recur for the left and right subtree
+    leafNodeDistance(node.left, path, set, dist)
+    leafNodeDistance(node.right, path, set, dist)
+
+    # remove the current node from the current path
+    path.remove(node)
+
+
+# Find all distinct nodes at a given distance from leaf nodes
+def printLeafNodeDistance(node, dist):
+    # list to store root-to-leaf path
+    path = []
+
+    # create an empty set to store distinct nodes at a given
+    # distance from leaf nodes
+    s = set()
+
+    # find all nodes
+    leafNodeDistance(node, path, s, dist)
+
+    # print output
+    print([e.val for e in s])
+
 # Driver Code 
 
 
