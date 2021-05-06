6bf0d5d350a099947e7781bd7b29b505fdde8a81
Sumit Singhal <sumitsinghal4390@gmail.com>
2021-05-06 22:39:31 +0530

    Update topological_sorting.py

--- topological_sorting.py
+++ topological_sorting.py
@@ -35,6 +35,41 @@ class Graph:
                 self.topological_sorting_util(v, visited, stack)
 
         print(stack[::-1])
+    
+        def compute_in_digree(self):
+        in_digree = {v:0 for v in self.graph}
+        for u in self.graph:
+            for v in self.graph[u]:
+                in_digree[v] += 1
+        return in_digree
+
+    # https://www.geeksforgeeks.org/topological-sorting-indegree-based-solution/
+    # This soln will do both (1. detect a cycle, 2. Topological sorting)
+    def topological_sorting_kahns(self):
+        in_digree = self.compute_in_digree()
+        sorted_vertices = []
+        queue = []
+        for v in self.graph:
+            if in_digree[v] == 0:
+                queue.append(v)
+        print(in_digree, self.graph)
+        count_nodes = 0
+        while queue:
+            node = queue.pop(0)
+            sorted_vertices.append(node)
+            for v in self.graph[node]:
+                in_digree[v] -= 1
+                if in_digree[v] == 0:
+                    queue.append(v)
+            # print(in_digree)
+            count_nodes += 1
+
+        print(in_digree)
+        # print(sorted_vertices, count_nodes)
+        if count_nodes == len(self.graph):
+            return sorted_vertices
+        else:
+            print("Graph has Cycle")
 
 
 if __name__ == '__main__':
